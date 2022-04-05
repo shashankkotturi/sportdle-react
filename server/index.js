71bfa669c8adac5d4a3acc42888b8fbde829cbb1
shashankkotturi <shashankkotturi@outlook.com>
2022-04-04 19:56:55 -0700

    Enable port flexibility again

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 console.log("Environment:", process.env.REDIRECT_URI)
 
 app.use(express.static(path.join(__dirname, "..", "client", "build")))
