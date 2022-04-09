00958e23a5238f6c8a30ab0cf38c820e26180d47
shashankkotturi <shashankkotturi@outlook.com>
2022-04-09 16:38:55 -0700

    A note for Spotify Premium account

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -6,9 +6,12 @@ const Login = () => {
   const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
   return (
+    <>
+    <p style={{color:"white"}}>This app only works if you have a Spotify Premium account!</p>
     <LoginButton>
       <LoginLink href={AUTH_URL}>Login with Spotify</LoginLink>
     </LoginButton>
+    </>
   );
 };
 
